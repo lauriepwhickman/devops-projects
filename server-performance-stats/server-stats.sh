f67723d86737fbba57ba6a5ec7ca779502c22886
Laurie Hickman <lauriehickman16@protonmail.com>
2025-11-14 18:56:12 +0000

    RAM usage

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-printf "###### Server Stats ######\n";
+echo "###################"
+echo "# Total CPU Usage #"
+echo "###################"
 
-printf "###### CPU Usage ######\n";
 top -bn1 | grep "$Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
+
+echo
+
+echo "###################"
+echo "# Total RAM Usage #"
+echo "###################"
+
+TOTAL_RAM=$(free -h | awk '{print $2}' | sed -n '2p');
+USED_RAM=$(free -h | awk '{print $3}' | sed -n '2p');
+FREE_RAM=$(free -h | awk '{print $4}' | sed -n '2p');
+printf "Total RAM: $TOTAL_RAM\n";
+printf "Used RAM: $USED_RAM\n";
+printf "Free RAM: $FREE_RAM\n";
+
+PERCENTAGE_USED=$((USED_RAM/FREE_RAM))
+
+printf "Percentage Used: $PERCENTAGE_USED %\n";
+printf "Percentage Free: \n";
