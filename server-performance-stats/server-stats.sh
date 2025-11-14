7f3ed31fde88a6c9e543d66b470d52d21bcfdb23
Laurie Hickman <lauriehickman16@protonmail.com>
2025-11-14 17:14:01 +0000

    CPU usage function

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-printf "Hello World";
+printf "###### Server Stats ######\n";
+
+printf "###### CPU Usage ######\n";
+top -bn1 | grep "$Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
